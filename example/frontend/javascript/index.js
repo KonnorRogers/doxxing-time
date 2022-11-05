7553fd2adc32925df5ef32781f6b1f61d8303342
Konnor Rogers <konnor5456@gmail.com>
2022-11-05 17:01:10 -0400

    minor fixes

--- example/frontend/javascript/index.js
+++ example/frontend/javascript/index.js
@@ -12,6 +12,7 @@ import "@shoelace-style/shoelace/dist/components/drawer/drawer"
 import "@shoelace-style/shoelace/dist/components/visually-hidden/visually-hidden"
 
 setBasePath("/shoelace-assets")
+
 // Uncomment the line below to add transition animations when Turbo navigates.
 // We recommend adding <meta name="turbo-cache-control" content="no-preview" />
 // to your HTML head if you turn on transitions. Use data-turbo-transition="false"
