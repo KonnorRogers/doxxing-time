6657cbe678bf957e767f832a6447101dd3ea058b
Konnor Rogers <konnor5456@gmail.com>
2023-06-17 19:09:29 -0400

    fix inspectors

--- example/plugins/builders/inspectors.rb
+++ example/plugins/builders/inspectors.rb
@@ -7,12 +7,16 @@ class Builders::Inspectors < SiteBuilder
       # This isn't great. but works for my case :shrug:
       document.css("main").css("h2[id],h3[id],h4[id],h5[id],h6[id]").each do |heading|
         text = heading.inner_text
-        heading.content = ""
-        anchor = %(
-          <a href='##{heading[:id]}'>#{text}</a>
-        )
 
-        heading << anchor
+        unless heading.css("a")
+          heading.content = ""
+          anchor = %(
+            <a href='##{heading[:id]}'>#{text}</a>
+          )
+
+          heading << anchor
+        end
+
 
         side_anchor = %(
           <a href='##{heading[:id]}' class='side-nav__link'>#{text}</a>
@@ -21,14 +25,15 @@ class Builders::Inspectors < SiteBuilder
         item = document.create_element("li", "", class: "side-nav__item")
         item << side_anchor
 
-        table_of_contents << item
+        # table_of_contents << item
 
         # we'll get here.
         # list = document.create_element("ul", "", class: "side-nav__category-menu")
         # list << item
         # mobile_menu.before list
-
       end
     end
   end
 end
+
+
